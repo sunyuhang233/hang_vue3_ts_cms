eb09eee4db0227fd15b8afeffe7386bb2ac2cde3
hang <2283582431@qq.com>
2023-11-14 16:23:50 +0800

    完成首页项目规范

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -8,7 +8,43 @@
     </MyCard>
     <MyCard title="技术栈"></MyCard>
     <MyCard title="项目结构"></MyCard>
-    <MyCard title="项目规范"></MyCard>
+    <MyCard title="项目规范">
+            <MyDescriptions
+        title="文件命名规范"
+        :column="1"
+        :table-datas="[
+          { name: '文件夹', description: '统一小写, 多个单词使用-分割' },
+          { name: '文件(.ts .vue .json .d.ts)', description: '统一小写, 多个单词使用-分割' }
+        ]"
+      />
+      <MyDescriptions
+        title="编写组件规范"
+        :column="1"
+        :table-datas="[
+          { name: '组件的文件', description: '统一小写, 多个单词使用-分割' },
+          {
+            name: '组件的目录结构',
+            description: '例如 button 组件：button/src/index.vue, 统一在button/index.ts导出'
+          },
+          {
+            name: '组件导包顺序',
+            description: '导vue技术栈的包 , 导第三方的工具函数 , 导本地的组件, 导本地的工具函数'
+          },
+          { name: '组件的名称', description: '统一大写开头，驼峰命名' },
+          { name: '组件属性顺序', description: 'name, components, props, emits, setup ...' },
+          { name: 'template标签', description: '小写加 - ( 例如：<case-panel/> )' },
+          {
+            name: 'template标签属性顺序',
+            description: 'v-if , v-for , ref, class, style, ... ,事件'
+          },
+          { name: '组件的props', description: '小写开头，驼峰命名，必须编写类型默认值' },
+          {
+            name: '组件的样式',
+            description: '作用域：scoped, lang = scss / less  ; 类名：统一小写, 多个单词使用-分割'
+          }
+        ]"
+      />
+    </MyCard>
     <MyCard title="Git提交规范">
       <MyDescriptions
       :column="1"
